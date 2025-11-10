8173ac241eb5aae88044b893b0c85c3b079afab3
sueasen <suehara.akihiko@gmail.com>
2025-11-10 10:41:35 +0000

    flask host_url を header (X-Forwarded) から生成に変更

--- flask-app/src/routes_auth.py
+++ flask-app/src/routes_auth.py
@@ -1,6 +1,10 @@
 from typing import Dict, Any
 from flask import Blueprint, jsonify, request, Response, redirect
 from supabase_auth_service import SupabaseAuthService
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 bp_auth = Blueprint("auth", __name__)
 supabase_auth_service = SupabaseAuthService()
@@ -9,7 +13,7 @@ supabase_auth_service = SupabaseAuthService()
 @bp_auth.route("/api/auth/register", methods=["POST"])
 def register() -> Response:
     data : Dict[str, Any]  = request.get_json() or {}
-    redirect_to : str = request.host_url.rstrip("/") + "/"
+    redirect_to : str = base_host_url()
     supabase_result, status_code = supabase_auth_service.signup(
         email=data.get("email"), 
         password=data.get("password"), 
@@ -50,7 +54,15 @@ def auth_user() -> Response:
 # GitHub認証リダイレクト
 @bp_auth.route("/api/auth/oauth2/github")
 def redirect_to_github() -> Response:
-    redirect_to : str = request.host_url.rstrip("/") + "/"
+    redirect_to : str = base_host_url()
     github_url : str = supabase_auth_service.get_github_signin_url(redirect_to=redirect_to)
     return redirect(github_url)
+
+# base_host_urlの作成
+def base_host_url() -> str:
+    host = request.headers.get("X-Forwarded-Host") or request.headers.get("Host")
+    scheme = (request.headers.get("X-Forwarded-Proto")
+              or request.scheme)
+    logger.info("base_host_url host=%s scheme=%s", host, scheme)
+    return f"{scheme}://{host}/"
 
