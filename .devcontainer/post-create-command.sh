22bc33521b6c7b6c218b9173211cdebc147277f0
sueasen <suehara.akihiko@gmail.com>
2025-11-10 10:26:02 +0000

    flask

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -5,6 +5,9 @@ echo "--- Running post-create-command.sh ---"
 
 # --- 実行したいコマンドを追記していきます ---
 
+# Python (Flask) の依存関係をインストール
+echo "--- Installing Python dependencies... ---"
+pip install --user -r flask-app/requirements.txt
 
 # --- スクリプト終了 ---
 echo "--- Setup completed successfully! ---"
---
+++ flask-app/src/routes_auth.py
@@ -0,0 +1,56 @@
+from typing import Dict, Any
+from flask import Blueprint, jsonify, request, Response, redirect
+from supabase_auth_service import SupabaseAuthService
+
+bp_auth = Blueprint("auth", __name__)
+supabase_auth_service = SupabaseAuthService()
+
+# アカウント登録
+@bp_auth.route("/api/auth/register", methods=["POST"])
+def register() -> Response:
+    data : Dict[str, Any]  = request.get_json() or {}
+    redirect_to : str = request.host_url.rstrip("/") + "/"
+    supabase_result, status_code = supabase_auth_service.signup(
+        email=data.get("email"), 
+        password=data.get("password"), 
+        redirect_to=redirect_to
+    )
+    if supabase_result.get("id"):
+        return jsonify({"message": "Registration successful. Please check your email for confirmation."}), 200
+    return jsonify(supabase_result), 400
+
+# ログイン
+@bp_auth.route("/api/auth/login", methods=["POST"])
+def login() -> Response:
+    data : Dict[str, Any]  = request.get_json() or {}
+    supabase_result, status_code = supabase_auth_service.signin_with_password(
+        email=data.get("email"), 
+        password=data.get("password")
+    )
+    return jsonify(supabase_result), status_code
+
+# ログアウト
+@bp_auth.route("/api/auth/logout", methods=["POST"])
+def logout() -> Response:
+    auth_header : str = request.headers.get("Authorization", "")
+    supabase_result, status_code = supabase_auth_service.signout(
+        access_token=auth_header[7:]
+    )
+    return jsonify({"message": "Logout successful."}), 200
+
+# 認証ユーザ情報取得
+@bp_auth.route("/api/auth/user")
+def auth_user() -> Response:
+    auth_header : str = request.headers.get("Authorization", "")
+    supabase_result, status_code = supabase_auth_service.get_user_by_access_token(
+        access_token=auth_header[7:]
+    )
+    return jsonify({"email": supabase_result.get("email")}), status_code
+
+# GitHub認証リダイレクト
+@bp_auth.route("/api/auth/oauth2/github")
+def redirect_to_github() -> Response:
+    redirect_to : str = request.host_url.rstrip("/") + "/"
+    github_url : str = supabase_auth_service.get_github_signin_url(redirect_to=redirect_to)
+    return redirect(github_url)
+
